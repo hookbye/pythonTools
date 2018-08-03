c69cfad8f298ae0823f5bd4abbc3b1f4b2e792f2
hookbye <weljun@sina.com>
2018-08-03 19:47:41 +0800

    更新git提交脚本

--- gitForQuickPush.py
+++ gitForQuickPush.py
@@ -1,15 +1,28 @@
-# -*- coding : utf-8 -*-
-
+#coding=utf-8
 import sys
 import os
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def gitCmd(cmd):
 	os.system("" + cmd)
 
 def gitPush():
 	gitCmd("git add -A")
-	gitCmd("git ci -m \"quick commit for save\"")
+	gitCmd("git st")
+	print "git add all done!"
+	print u"输入提交日志:",
+	commitInfo = raw_input()
+	if commitInfo == "":
+		commitInfo = "\"quick commit for save\""
+	gitCmd("git ci -m "+commitInfo)
+	print "git commit done!"
 	gitCmd("git pl")
 	gitCmd("git ps")
+	print "git push done!"
 if __name__ == '__main__':
-	gitPush()
+	try:
+		gitPush()
+	except Exception,ex:
+		print ex
+		raw_input()
