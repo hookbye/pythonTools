7949e706412720db577f9bb2cb25340e41ec2f75
hookbye <weljun@sina.com>
2018-08-07 10:22:37 +0800

    增加svn快速提交脚本

---
+++ svnQuickUpdate.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def svnCmd(cmd):
+	os.system("" + cmd)
+
+def svnUp():
+	svnCmd("svn update")
+	print "update done!"
+if __name__ == '__main__':
+	try:
+		svnUp()
+		print "enter to quit!"
+		raw_input()
+	except Exception,ex:
+		print ex
